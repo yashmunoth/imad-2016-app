46ec55d913488c7ca7301e9b5b02de1584f1c476
yashmunoth <yash.munoth@gmail.com>
2016-11-21 01:22:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,17 +78,7 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
-});
 
-app.get('/article-two', function(req,res){
-    res.send('Article two is being served');
-});
-
-app.get('/article-three', function(req,res){
-    res.send('Article three is being served');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
