937ed6b81688df96771fb38f92edbfb56485d049
yashmunoth <yash.munoth@gmail.com>
2016-11-20 21:32:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-    user: 'yashmunoth',
-    database: 'yashmunoth',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
-    password: process.env.DB_PASSWORD
-};
 
 var app = express();
 app.use(morgan('combined'));
