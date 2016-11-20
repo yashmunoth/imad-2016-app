5a0061a49f2c02f430a089f0afd3c6761ce9da42
yashmunoth <yash.munoth@gmail.com>
2016-11-20 22:50:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+var config = {
+    user: 'yashmunoth',
+    database: 'yashmunoth',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
