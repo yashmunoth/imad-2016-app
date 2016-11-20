e16dcd0951f184beecd0c98821fec8718607cc51
yashmunoth <yash.munoth@gmail.com>
2016-11-20 20:32:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function() {
-    counter=counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                var counter=request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   request.open('GET','http://yashmunoth.imad.hasura-app.io/counter',true);
+   request.send(null);
         
     };
